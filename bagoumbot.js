8243310f427a079b966d66ab7388774eec5e84db
Keegan Parker <keegandparker@gmail.com>
2016-11-11 20:08:49 -0800

    Fix idle urls, refactor to camelCase and split functionality into functions

--- bagoumbot.js
+++ bagoumbot.js
@@ -5,31 +5,24 @@ var bot = new Discord.Client();
 
 var loginToken = process.env.DISC_TOKEN;
 var prefix = "$";
-var card_data = {}
+var cardData = {}
 
 bot.on("message", msg => {
     if (msg.content.startsWith(prefix) &&
         msg.content.length > 1 && !msg.author.bot) {
-        let args = msg.content.substring(1).split(" ");
-        let command = args[0];
-        console.log("Executing:", msg.content);
-        if (command == "echo") {
-            msg.channel.sendMessage(args.slice(1).join(" "));
-        } else if (command == "card-name") {
-            let subname = args.slice(1).join(" ").toLowerCase();
-            let card_names = Object.keys(card_data).filter(function(name) {
-                return name.includes(subname);
-            });
-            output_cards(msg, card_names);
-        } else if (command == "card-search") {
-            let card_names = Object.keys(card_data);
-            for (var i = 1; i < args.length; i++) {
-                let term = args[i].toLowerCase();
-                card_names = card_names.filter(function(card_name) {
-                    return does_term_match_card(term, card_name);
-                });
+        try {
+            let args = msg.content.substring(1).split(" ");
+            let command = args[0];
+            console.log("Executing:", msg.content);
+            if (command == "card-name") {
+                cardNameCommand(args, msg);
+            } else if (command == "card-search") {
+                cardSearchCommand(args, msg);
             }
-            output_cards(msg, card_names);
+        } catch (err) {
+            console.log(
+                "Had error processing", msg.content, " error:", err
+            );
         }
     }
 });
@@ -48,10 +41,29 @@ bot.on("disconnected", () => {
     bot.login(loginToken);
 });
 
-function send_formatted_card(msg, card_name) {
-    let card = card_data[card_name];
-    let get_idle_url = "http://www.bagoum.com/getIdle/" + encodeURIComponent(card_name);
-    request(get_idle_url, function(err, resp, idle_animation_url) {
+function cardNameCommand(args, msg) {
+    let subname = args.slice(1).join(" ").toLowerCase();
+    let cardNames = Object.keys(cardData).filter(function(name) {
+        return name.includes(subname);
+    });
+    outputCards(msg, cardNames);
+};
+
+function cardSearchCommand(args, msg) {
+    let cardNames = Object.keys(cardData);
+    for (var i = 1; i < args.length; i++) {
+        let term = args[i].toLowerCase();
+        cardNames = cardNames.filter(function(cardName) {
+            return doesTermMatchCard(term, cardName);
+        });
+    }
+    outputCards(msg, cardNames);
+};
+
+function sendFormattedCard(msg, cardName) {
+    let card = cardData[cardName];
+    let getIdleUrl = "http://www.bagoum.com/getIdle/" + encodeURIComponent(card.name);
+    request(getIdleUrl, function(err, resp, idleAnimationUrl) {
         if (err) {
             console.log(err);
             return;
@@ -60,56 +72,56 @@ function send_formatted_card(msg, card_name) {
             console.log("Unexpected response code:", resp.statusCode);
             return;
         }
-        formatted_text = card.name + "   " + card.manaCost + "\n" + 
+        formattedText = card.name + "   " + card.manaCost + "\n" + 
             card.shortfaction + " " + card.type + " - " + card.expansion + "\n" +  
             card.description + "\n";
         if (card.type === "Unit") {
-            formatted_text += card.attack + "/" + card.health + "\n";
+            formattedText += card.attack + "/" + card.health + "\n";
         }
-        formatted_text += idle_animation_url;
-        msg.channel.sendMessage(formatted_text);
+        formattedText += "http://" + idleAnimationUrl;
+        msg.channel.sendMessage(formattedText);
     });
-}
+};
 
-function output_cards(msg, card_names) {
-    if (card_names.length == 1) {
-        send_formatted_card(msg, card_names[0]);
-    } else if (card_names.length > 1) {
+function outputCards(msg, cardNames) {
+    if (cardNames.length == 1) {
+        sendFormattedCard(msg, cardNames[0]);
+    } else if (cardNames.length > 1) {
         msg.channel.sendMessage(
             "Too many matches found, pm'ing you the names that matched your query"
         );
         msg.author.sendMessage(
-            card_names.join(", ")
+            cardNames.join(", ")
         );
     } else {
         msg.channel.sendMessage(
             "Sorry, but I can't find a card with those parameters."
         );
     }
-}
+};
 
-function does_term_match_card(term, card_name) {
-    let card = card_data[card_name];
+function doesTermMatchCard(term, cardName) {
+    let card = cardData[cardName];
     return card.searchableText.indexOf(term) > -1;
-}
+};
 
-function split_searchable_text(searchable_text) {
+function splitSearchableText(searchableText) {
     let re = /([0-9a-z])([A-Z])|([a-z])([0-9])|([0-9])([a-z])/g;
-    searchable_text = searchable_text.replace(re, '$1$3$5 $2$4$6');
-    return searchable_text.split(/[ .,]/).map(function(term) {
+    searchableText = searchableText.replace(re, '$1$3$5 $2$4$6');
+    return searchableText.split(/[ .,]/).map(function(term) {
         return term.toLowerCase()
     });
 };
 
-function build_card_data(cards) {
-    for (var card_name in cards) {
-        if (!cards.hasOwnProperty(card_name)) {
+function buildCardData(cards) {
+    for (var cardName in cards) {
+        if (!cards.hasOwnProperty(cardName)) {
             continue;
         }
-        card = cards[card_name];
-        card.searchableText = split_searchable_text(card.searchableText);
-        card.name = card_name;
-        card_data[card_name.toLowerCase()] = card
+        card = cards[cardName];
+        card.searchableText = splitSearchableText(card.searchableText);
+        card.name = cardName;
+        cardData[cardName.toLowerCase()] = card
     }
 };
 
@@ -124,6 +136,6 @@ request("http://bagoum.com/cardsFullJSON", function(err, resp, body) {
         return;
     }
     var cards = JSON.parse(body);
-    build_card_data(cards);
+    buildCardData(cards);
     bot.login(loginToken);
 });
